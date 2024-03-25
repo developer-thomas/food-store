ce34d5d251a3c4066516e56b9457f0a084cbda21
developer-thomas <andepipous@gmail.com>
2024-03-25 19:09:59 -0400

    features: create home component

---
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FoodService } from 'src/app/services/food.service';
+import { Food } from 'src/app/shared/models/Food';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {
+  public foods: Food[];
+
+  constructor(private foodService: FoodService) {
+    this.foods = foodService.getAll();
+  }
+}
