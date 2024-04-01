412038edea2be53a169bc4453b02b1f3d5011b67
developer-thomas <andepipous@gmail.com>
2024-04-01 06:12:26 -0400

    features: create the partial tags component

---
+++ frontend/src/app/components/partials/tags/tags.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { FoodService } from 'src/app/services/food.service';
+
+import { Tag } from 'src/app/shared/models/Tag';
+
+@Component({
+  selector: 'app-tags',
+  templateUrl: './tags.component.html',
+  styleUrl: './tags.component.scss',
+})
+export class TagsComponent {
+  public tags!: Tag[];
+  constructor(private foodService: FoodService) {
+    this.tags = foodService.getAllTags();
+  }
+}
