a57a25ee6b5cb3cd91615d383408d2d12e6f4eaa
developer-thomas <andepipous@gmail.com>
2024-04-01 06:14:51 -0400

    features: create food page component wich is render when the user clicks on the desired food

---
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FoodService } from 'src/app/services/food.service';
+import { Food } from 'src/app/shared/models/Food';
+
+@Component({
+  selector: 'app-food-page',
+  templateUrl: './food-page.component.html',
+  styleUrl: './food-page.component.scss',
+})
+export class FoodPageComponent {
+  public food!: Food;
+
+  constructor(
+    activatedRoute: ActivatedRoute,
+    private foodService: FoodService
+  ) {
+    activatedRoute.params.subscribe((params) => {
+      if (params.id) {
+        this.food = foodService.getFoodById(params.id);
+        console.log(this.food);
+      }
+    });
+  }
+}
+
+// parei no minuto 4
