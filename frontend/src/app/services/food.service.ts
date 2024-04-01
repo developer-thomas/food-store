311927ede1dc9370fd8bc42a7f2ac54b935fb2d1
developer-thomas <andepipous@gmail.com>
2024-04-01 06:10:41 -0400

    features: add filter logic by tag name to the service

--- frontend/src/app/services/food.service.ts
+++ frontend/src/app/services/food.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Input } from '@angular/core';
 import { Food } from '../shared/models/Food';
-import { sample_foods } from '../data';
+import { sample_foods, sample_tags } from '../data';
+import { Tag } from '../shared/models/Tag';
 @Injectable({
   providedIn: 'root',
 })
@@ -15,5 +16,25 @@ export class FoodService {
     return this.getAll().filter((food) =>
       food.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
     );
+  }
+
+  getFoodById(foodId: string): Food {
+    return this.getAll().find((food) => food.id == foodId) ?? new Food();
+  }
+
+  public getAllTags(): Tag[] {
+    return sample_tags;
+  }
+
+  public getAllFoodsByTag(tag: string): Food[] {
+    if (tag == 'All') {
+      return this.getAll();
+    } else {
+      return (
+        this.getAll().filter((food) =>
+          food.tags?.toLowerCase().includes(tag.toLowerCase())
+        ) ?? new Tag()
+      );
+    }
   }
 }
