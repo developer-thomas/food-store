39d01eef07b3cd8bd565d49cb59b0d1f8210625e
developer-thomas <andepipous@gmail.com>
2024-04-22 06:40:45 -0400

    features: update the mock data with users list

--- backend/src/data.ts
+++ backend/src/data.ts
@@ -124,3 +124,20 @@ export const sample_tags: any[] = [
   { name: "Fry", count: 1 },
   { name: "Soup", count: 1 },
 ];
+
+export const sample_users: any[] = [
+  {
+    name: "John Doe",
+    email: "john@gmail.com",
+    password: "12345",
+    address: "Toronto On",
+    isAdmin: true,
+  },
+  {
+    name: "jane Doe",
+    email: "jane@gmail.com",
+    password: "12345",
+    address: "Shangai",
+    isAdmin: false,
+  },
+];
