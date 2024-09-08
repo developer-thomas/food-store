dcdd17a27968320f6c99220ec5d96bad692c4588
Thomas Edson <andepipous@gmail.com>
2024-09-08 13:07:35 -0400

    feat: integrate backend to mongodb atlas

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,27 @@
 import express from "express";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 import foodRouter from "./routers/food.router";
 import userRouter from "./routers/user.router";
+import dotenv from "dotenv";
+dotenv.config();
+import { dbConnect } from "./configs/database.config";
 
 const app = express();
 
-app.use("/api/foods", foodRouter);
-app.use("api/users", userRouter);
-
+dbConnect();
 // Habilitar o uso de JSON no express, por padrão não aceita json
 app.use(express.json());
-
 // configuração do middleware cors para conseguirmos usar o front na porta 4200 sem erro de cors
-app.use(
-  cors({
-    credentials: true,
-    origin: ["http://localhost:4200"],
-  })
-);
+
+const corsOptions: CorsOptions = {
+  origin: "http://localhost:4200",
+  methods: "GET, POST, PATCH, PUT, DELETE",
+};
+
+app.use(cors(corsOptions));
+
+app.use("/api/foods", foodRouter);
+app.use("/api/users", userRouter);
 
 const port = 5000;
 
